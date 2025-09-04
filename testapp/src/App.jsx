8accfbaace2b3ed6edda5ea84bbc1cd21aada90d
piwkom <piwkom44@gmail.com>
2025-09-04 13:37:58 +0200

    asefhjasejhaseg

--- testapp/src/App.jsx
+++ testapp/src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [course, setCourse] = useState(["Programowanie w C#","Angular dla początkujących","Kurs Django"])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Liczba kursow {course.length}</h2>
+      <table>
+        {course.map(s => (
+          <tr>{s.id}. {s}</tr>
+        ))}
+      </table>
+      <form>
+        <label for="nameInput">Imię i Nazwisko:</label>
+        <input type="text" name="nameInput"></input>
+        <label for="numberInput">Numer kursu:</label>
+        <input type="number" name="numberInput"></input>  
+        <button type="button">Zapisz do kursu</button>
+      </form>
     </>
   )
 }
