bfa3bb11ea97eb365fc238002593079fc8aec7f5
piwkom <piwkom44@gmail.com>
2025-09-11 09:29:48 +0200

    .

--- TestApp/testapp/src/App.jsx
+++ TestApp/testapp/src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react'
 
 
 function App() {
- 
+  const [courses,setCourse] = useState(["Programowanie w C#","Angular dla poczatkujacych","Kurs Django"])
 
+  const CourseComponent=()=>{
+    
+    return(
+      <>
+      <h2>Liczba kursow: {courses.length}</h2>
+      <ol>
+      {courses.map(n=>(
+        <li>{n}</li>
+      ))}
+      </ol>
+      </>
+    )
+  }
+  
   return (
     <>
-      
+    <CourseComponent/>
     </>
   )
 }
