56cab261a9f17f1f35735ccee704c6470833a538
piwkom <piwkom44@gmail.com>
2025-09-18 09:58:50 +0200

    arkusz 2022_06 done

---
+++ INF04_22_6/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css' ;
+
+function App() {
+  const [courses, setCourses] = useState(["Programowanie w C#","Angular dla poczatkujacych","Kurs Django"])
+  const [name, setName] = useState('')
+  const [num, setNum] = useState(0)
+  
+  const NameChange = (e) => {
+    setName(e.target.value)
+  }
+  const NumChange = (e) =>{
+    setNum(e.target.value)
+  }
+
+  const OverrideSubmit = (e) =>{
+    e.preventDefault()
+    console.log(name)
+    if(num > (courses.length)-1 || num<0){
+      console.log("nieprawidlowy numer kursu")
+    } else {
+      console.log(courses[num-1])
+    }
+  }
+
+  return (
+    <>
+    <h2>Liczba Kursow: {courses.length}</h2>
+    <ol>
+      {courses.map(course => (
+        <li key={course}>{course}</li>
+      ))}
+    </ol> 
+      <form className="form-group" onSubmit={OverrideSubmit}>
+        <label htmlFor="NameInput" className="">Imie i nazwisko:</label>
+        <input className="form-control" name="NameInput" type="text" onChange={NameChange}/>
+        <label htmlFor="NumberInput" className="">Numer kursu:</label>
+        <input className="form-control" min="1" max={courses.length} name="NumberInput" type="number" onChange={NumChange}/>
+        <br/><button className="btn btn-primary" type="submit">Zapisz do kursu</button>        
+      </form>
+    </>
+  )
+}
+
+export default App
