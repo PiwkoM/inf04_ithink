4b2df35a022051323f77330274ce783fdb2af3dd
piwkom <piwkom44@gmail.com>
2025-10-16 21:53:19 +0200

    screw this accordion breh

--- sprawdzian/src/App.jsx
+++ sprawdzian/src/App.jsx
@@ -8,30 +8,41 @@ function App() {
   {id:3, title: "Jak mogę skontaktować się z pomocą techniczną?", content:'Aby rozpocząć pracę z aplikacją, wystarczy kliknąć na przycisk "Start" w głównym menu. Następnie możesz przejść przez krótki przewodnik, który pokaże Ci wszystkie dostępne funkcje. Aplikacja jest intuicyjna i łatwa w obsłudze.'}
   ])
 
+
+  const[activeItem,setActive] = useState(null)
+
+  const HandleClick = (itemID) => {
+      setActive(activeItem === itemID ? null : itemID) // self explanatory
+  }
+
   return (
     <>
       <br/>
       <div className="container-sm sb-4">
-
+      {/* i still have no clue what sb-4 is but might aswell put it everywhere lmao */}
+      <div className="accordion" id="accordionExample">
       {array.map( item => (
-      <div class="accordion" id="accordionExample">
-        <div class="card">
-          <div class="card-header" id="heading">
-            <h5 class="mb-0">
-              <button class="btn btn-link" type="button" data-toggle="collapse" data-target={"#collapse"+item.id}
-              aria-expanded="false" aria-controls={"collapse"+item.id}>
+        <div className="card">
+          <div className="card-header" id="heading">
+            <h5 className="mb-0">  {/* l-22 ditto */}
+              <button className="btn btn-link" type="button"
+              aria-expanded={activeItem === item.id ? 'true' : 'false'} aria-controls={"collapse"+item.id} onClick={() => HandleClick(item.id)}>
+                {/* 
+                react functions conflict with bootstraps data-toggle stuff or sth and im not learning this stupid libraries' special attributes
+                */}
                 {item.title}
               </button>
             </h5>
-          </div>
-          <div id={"collapse"+item.id} class="collapse" data-parent="#accordionExample">
-            <div class="card-body">
+          </div>    
+          {/*i completely forgot about the $ trick in most languages, too bad i'll forget about it later on anyway*/}
+          <div id={"collapse"+item.id} className={`collapse ${activeItem === item.id ? "show" : ""}`} data-parent="#accordionExample">
+            <div className="card-body">
               {item.content}
             </div>
           </div>
         </div>
-      </div>
       ))}
+      </div>
 
       <br/>
       <div className="card">
@@ -55,7 +66,18 @@ function App() {
           <h5>Podsumowanie</h5>
         </div>
         <div className="card-body row">
-
+          <div className="col-md-4">
+            <h5 className="text-primary">{array.length}</h5>
+            <h5 className="text-muted">Pytania</h5>
+          </div>
+          <div className="col-md-4">
+            <h5 className="text-info">{activeItem ? 1 : 0}</h5>
+            <h5 className="text-muted">Otwarte</h5>
+          </div>
+          <div className="col-md-4">
+            <h5 className="text-success">{array.length - (activeItem ? 1 : 0)}</h5>
+            <h5 className="text-muted">Zamknięte</h5>
+          </div>
         </div>
       </div>
 
